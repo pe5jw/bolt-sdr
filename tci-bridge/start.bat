39b8e84b032005472fb83c4b38900db539fd5603
Joeri <pe5jw@noppus.nl>
2026-08-27 23:48:21 +0200

    fix: tci-bridge venv in AppData/Local/Bolt

--- tci-bridge/start.bat
+++ tci-bridge/start.bat
@@ -1,10 +1,8 @@
 @echo off
 title Bolt SDR TCI Bridge
 cd /d "%~dp0"
-set PYTHON=%~dp0venv\bin\python.exe
-if not exist "%PYTHON%" (
-    set PYTHON=C:\msys64\mingw64\bin\python.exe
-)
+set PYTHON=%LOCALAPPDATA%\Bolt\tci-venv\bin\python.exe
+if not exist "%PYTHON%" set PYTHON=C:\msys64\mingw64\bin\python.exe
 echo.
 echo === Bolt SDR TCI Bridge ===
 echo CAT poort : 4532  (N1MM+: Kenwood TS-2000 / 0.0.0.0:4532)
