499aaf00606115d82556a61b8f158d68580f1c2f
Joeri <pe5jw@noppus.nl>
2026-08-31 21:31:52 +0200

    feat: setup-bolt.cmd voor eerste installatie (venv, firewall)

---
+++ setup-bolt.cmd
@@ -0,0 +1,20 @@
+@echo off
+title Bolt SDR Setup
+echo === Bolt SDR Setup ===
+echo.
+
+echo Aanmaken TCI bridge Python omgeving...
+C:\msys64\mingw64\bin\python.exe -m venv "%LOCALAPPDATA%\Bolt\tci-venv"
+"%LOCALAPPDATA%\Bolt\tci-venv\bin\python.exe" -m pip install -q websockets
+echo TCI bridge klaar.
+
+echo.
+echo Firewall regels aanmaken...
+netsh advfirewall firewall add rule name="Bolt SDR HTTPS 6443" dir=in action=allow protocol=TCP localport=6443 > nul 2>&1
+netsh advfirewall firewall add rule name="Bolt SDR HTTP 6061" dir=in action=allow protocol=TCP localport=6061 > nul 2>&1
+netsh advfirewall firewall add rule name="Bolt SDR TCI 40001" dir=in action=allow protocol=TCP localport=40001 > nul 2>&1
+echo Firewall klaar.
+
+echo.
+echo Setup voltooid! Start Bolt SDR met start-bolt.cmd
+pause
